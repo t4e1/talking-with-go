627b1b33c9eadacbb5085595d77f2dda8eb1699d
t4e1 <jtw5738@naver.com>
2025-08-02 23:53:18 +0900

    Http connection practice

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+var request struct { 
+	
+}
+
 func main() {
-	// Statrt !
+
+	// router
+	router := gin.Default()
+
+	// A client communicate with LLM at this end point
+	router.POST("/queries", sendToLLM)
+
+	// Run server as localhost:8080
+	router.Run(":8080")
+}
+
+func sendToLLM(c *gin.Context) {
+
+	fmt.Println(c.))
+	c.JSON(200, gin.H{
+		"message": "Hello",
+	})
+	fmt.Println("Communicate Success")
 }
